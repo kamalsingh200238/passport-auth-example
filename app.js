d7434954c05164ef6aff9b7e472b345e4a851df6
Kamal Singh <kamalsingh200238@gmail.com>
2023-04-10 22:14:17 +0530

    final commit

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+var session = require('express-session');
+var passport = require('passport');
+
+var SQLiteStore = require('connect-sqlite3')(session);
 
 var indexRouter = require("./routes/index");
 var authRouter = require("./routes/auth");
@@ -22,6 +26,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false,
+  store: new SQLiteStore({ db: 'sessions.db', dir: './var/db' })
+}));
+app.use(passport.authenticate('session'));
 
 app.use("/", indexRouter);
 app.use("/", authRouter);
